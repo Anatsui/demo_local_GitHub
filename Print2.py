1bf4a18a7eec483dcefbccb9e1b2df973cb5f639
Anatsui <151625015+Anatsui@users.noreply.github.com>
2025-11-29 13:02:41 +0000

    Update print statement in Print2.py

--- Print2.py
+++ Print2.py
@@ -1,3 +1,3 @@
 print("This is Print2.py from VS code!")
 print("Hello from Print2.py!")
-print("Print another line of code")
+print("Print another line of code from GitHub and pull it to the local repo")
